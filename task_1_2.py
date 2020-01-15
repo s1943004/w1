43ee92ce3c6141fb7492473316b0ea7ea30412d9
Yiwen Zhou <s1943004@baltic10.geos.ed.ac.uk>
2020-01-15 23:52:17 +0000

    task_1_2

--- task_1_2.py
+++ task_1_2.py
@@ -1,2 +1,31 @@
+""" This is the main function.
+    prerequisite:
+    based on Python 3.6
+    
+    Parameters
+    ---------
+    n: int
+        the number of rows or columns of the chessboard
+"""
+
+
+############################
 from math import sqrt
-def distance(p1,p2):
+############################
+
+def distance(n):
+    """
+
+    """
+    for i in range (1,n+1):
+        for j in range(1,n+1):
+            for k in range(1,n+1):
+                for m in range(1,n+1):
+                    if ((k==i) & (j==m)): #avoid calculating the distance from a square to itself
+                        continue
+                    else:
+                        distance = sqrt((k-i)**2+(j-m)**2)
+                        print('{0},{1} to {2},{3} distance {4}'.format(i,j,k,m,distance))
+                
+if __name__ == '__main__':
+    distance(8)
